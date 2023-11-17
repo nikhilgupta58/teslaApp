6bfb6eb97ab109bc027013b4b2a42a276c85963a
Nikhil <nikhil@tartanhq.com>
2023-11-17 11:40:00 +0530

    profile icon

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -3,13 +3,14 @@ import {Pressable} from 'react-native';
 import LeftArrowIcon from '../../icons/LeftArrowIcon';
 import LockIcon from '../../icons/LockIcon';
 import OpenLockIcon from '../../icons/OpenLockIcon';
+import ProfileIcon from '../../icons/ProfileIcon';
 import SettingIcon from '../../icons/SettingIcon';
 import DefaultState from './wrapper/DefaultState';
 import PressedState from './wrapper/PressedState';
 
 interface IProp {
   type?: 'default' | 'pressed';
-  icon: 'leftArrow' | 'settings' | 'lock' | 'unLock';
+  icon: 'leftArrow' | 'settings' | 'lock' | 'unLock' | 'profile';
   onPress?: () => void;
 }
 
@@ -23,6 +24,7 @@ const Icon = {
   settings: SettingIcon,
   lock: LockIcon,
   unLock: OpenLockIcon,
+  profile: ProfileIcon,
 };
 
 export default function Button({
