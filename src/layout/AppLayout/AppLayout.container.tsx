e7813438de3cbaeb303c1a961a211a71678035f5
Nikhil <nikhil@tartanhq.com>
2023-11-24 12:13:32 +0530

    climate

--- src/layout/AppLayout/AppLayout.container.tsx
+++ src/layout/AppLayout/AppLayout.container.tsx
@@ -9,7 +9,7 @@ const screens = [DashboardScreenRoute, ChargingScreenRoute];
 export default function AppLayoutContainer({children}: any) {
   const route = useRoute();
   const currentScreenName = route.name;
-  const showNav = screens?.includes(currentScreenName);
+  const showNav = true || screens?.includes(currentScreenName);
   return (
     <AppLayoutContext.Provider value={{children, showNav}}>
       <AppLayoutView />
--- src/utils/route.ts
+++ src/utils/route.ts
@@ -36,6 +36,7 @@ export const routes = [
     component: Climate,
     option: {
       headerShown: false,
+      animationEnabled: false,
     },
   },
 ];
